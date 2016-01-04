db4e1aca97d5fc5cdee7e5e59b4d68cd7f5bf6ee
sparklyballs <sparklyballs@gmail.com>
2016-01-04 05:18:59 +0000

    add set folders routine

--- init/50_setup.sh
+++ init/50_setup.sh
@@ -6,3 +6,17 @@ cd /config/www/bgpanel
 git pull
 chown -R abc:abc /config
 
+if [ ! -d "/config/log/myqsl" ]; then
+mkdir -p /config/log/mysql
+fi
+
+
+if [ ! -d "$DATADIR" ]; then
+mkdir -p "$DATADIR"
+fi
+
+if [ ! -d "/var/run/mysqld" ]; then
+mkdir -p /var/run/mysqld
+chmod -R 777 /var/run/mysqld
+fi
+
